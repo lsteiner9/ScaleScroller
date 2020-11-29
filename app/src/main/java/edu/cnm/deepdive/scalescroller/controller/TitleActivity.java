d393d2464530867a6bb173b40b8da9dcfdc605e0
lsteiner9 <lsteiner1@cnm.edu>
2020-11-28 19:56:52 -0700

    Navigation is working as intended.

--- app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleActivity.java
+++ app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleActivity.java
@@ -1,39 +1,18 @@
 package edu.cnm.deepdive.scalescroller.controller;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import edu.cnm.deepdive.scalescroller.MobileNavigationDirections;
-import edu.cnm.deepdive.scalescroller.R;
 import edu.cnm.deepdive.scalescroller.databinding.ActivityTitleBinding;
-import edu.cnm.deepdive.scalescroller.service.GoogleSignInService;
 
 public class TitleActivity extends AppCompatActivity {
 
   private ActivityTitleBinding binding;
-  private GoogleSignInService signInService;
-  private NavController navController;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    signInService = GoogleSignInService.getInstance();
     binding = ActivityTitleBinding.inflate(getLayoutInflater());
-    binding.playingAs.setText(getString(R.string.playing_as,
-        signInService.getAccount().getDisplayName()));
-    binding.logoutButton.setOnClickListener((v) -> logout());
     setContentView(binding.getRoot());
-    navController = ((NavHostFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.fragment_container_view)).getNavController();
-    binding.learnButton.setOnClickListener((v) ->
-        navController.navigate(MobileNavigationDirections.openScaleSelect()));
-    // TODO how to pass arguments here? Pass in a bundle?
-    binding.challengeButton.setOnClickListener((v) ->
-        navController.navigate(MobileNavigationDirections.openChallengeModeGame()));
-    binding.settingsButton.setOnClickListener(
-        (v) -> navController.navigate(MobileNavigationDirections.openSettings()));
   }
 
   @Override
@@ -41,12 +20,4 @@ public class TitleActivity extends AppCompatActivity {
     super.onPostCreate(savedInstanceState);
   }
 
-  private void logout() {
-    GoogleSignInService.getInstance().signOut()
-        .addOnCompleteListener((ignored) -> {
-          Intent intent = new Intent(this, LoginActivity.class)
-              .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-          startActivity(intent);
-        });
-  }
 }
--- app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleFragment.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.scalescroller.controller;
 
 import android.annotation.SuppressLint;
 
+import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -16,22 +17,49 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import edu.cnm.deepdive.scalescroller.R;
 import edu.cnm.deepdive.scalescroller.databinding.FragmentTitleBinding;
+import edu.cnm.deepdive.scalescroller.service.GoogleSignInService;
 
 public class TitleFragment extends Fragment {
 
   FragmentTitleBinding binding;
+  private GoogleSignInService signInService;
+  private NavController navController;
+
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    binding = FragmentTitleBinding.inflate(inflater);
-    return binding.getRoot();
+    signInService = GoogleSignInService.getInstance();
+    binding = FragmentTitleBinding.inflate(getLayoutInflater());
+    binding.playingAs.setText(getString(R.string.playing_as,
+        signInService.getAccount().getDisplayName()));
+    binding.logoutButton.setOnClickListener((v) -> logout());
+    navController = NavHostFragment.findNavController(this);
+    binding.learnButton.setOnClickListener((v) ->
+        navController.navigate(TitleFragmentDirections.openScaleSelect()));
+    // TODO how to pass arguments here? Pass in a bundle?
+    binding.challengeButton.setOnClickListener((v) ->
+        navController.navigate(TitleFragmentDirections.openChallengeModeGame()));
+    binding.settingsButton.setOnClickListener(
+        (v) -> navController.navigate(TitleFragmentDirections.openSettings()));    return binding.getRoot();
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+  }
+
+  private void logout() {
+    GoogleSignInService.getInstance().signOut()
+        .addOnCompleteListener((ignored) -> {
+          Intent intent = new Intent(getActivity(), LoginActivity.class)
+              .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+          startActivity(intent);
+        });
   }
 }
