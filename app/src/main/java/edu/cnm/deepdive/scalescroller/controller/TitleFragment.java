bc10d6c8104cee48b1dc522ea6b9d15fbd714ebe
lsteiner9 <lsteiner1@cnm.edu>
2020-11-30 19:15:08 -0700

    More layout work.

--- app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleFragment.java
+++ app/src/main/java/edu/cnm/deepdive/scalescroller/controller/TitleFragment.java
@@ -24,7 +24,6 @@ public class TitleFragment extends Fragment {
   private GoogleSignInService signInService;
   private NavController navController;
 
-
   /**
    * Initializes display text and navigation.
    *
